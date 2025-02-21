9e05e4245cc8e35edec68c519fe9b5b7572744b0
E <ek@MacBook-Air-E.local>
2025-02-21 18:21:28 +0200

    Fix webhook and Gunicorn startup

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-gunicorn -w 4 -b 0.0.0.0:$PORT kvn:app
 
+# Uzstāda webhook, lai Telegram zina, kur sūtīt atbildes
+python3 -c 'import asyncio; from kvn import set_webhook; asyncio.run(set_webhook())'
+
+# Palaiž Gunicorn ar Flask aplikāciju (flask_app)
+exec gunicorn --bind 0.0.0.0:5000 kvn:flask_app
